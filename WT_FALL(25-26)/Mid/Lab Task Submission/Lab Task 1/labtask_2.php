5dd2313064bbf21cd3886450367c31141ffb8bc5
Haider Rabbi <129550772+rabbi2580@users.noreply.github.com>
2025-11-17 11:58:48 +0600

    Add files via upload

---
+++ WT_FALL(25-26)/Mid/Lab Task Submission/Lab Task 1/labtask_2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{
+                    font-family: 'Times New Roman', Times, serif;
+                    padding: 10px;
+                    background-color: white;
+                    color: cyan;
+                    
+                }
+            h2,h1{
+                text-align: center;
+            }
+            .section-box{
+                background-color:white;
+                padding: 15px;
+                width: 200px;
+                margin: auto;
+                border-radius: 10px;
+            }
+            input,button{
+                width: 100%;
+                padding:5px;
+                margin: auto;
+
+            }
+            button{
+                background-color: blue;
+                color: white;
+                cursor: pointer;
+            }
+            #error{
+                color: red;
+                text-align: center;
+                margin-top: auto;
+            }
+            .activity{
+                background:#e8e8e8 ;
+                padding: 15px;
+                margin-top: auto;
+                border-radius: 10px;
+                
+            }
+            .remove_button {
+                background:red ;
+                padding: 15px;
+                margin-top: auto;
+                border-radius: 10px;
+                cursor: pointer;
+
+
+            }
+            </body>
+
+    </head>
+</html>
